34f6c20635bcf7a5a612b33c37cb05df8d198299
Alessandro Marchesini <149405466+marchesinia187179@users.noreply.github.com>
2025-12-03 09:23:04 +0100

    Refactor file name definitions and main function calls
    Updated file name definitions for day modules and adjusted main function calls accordingly.

--- aoc25/main.c
+++ aoc25/main.c
@@ -1,8 +1,11 @@
 #include "./day1/day_one.h"
+#include "./day2/day_two.h"
 
-#define FILE_NAME "input.txt"
+#define FILE_NAME_DAY1 "day1/input.txt"
+#define FILE_NAME_DAY2 "day2/input.txt"
 int main(void) {
-    day_one_main(FILE_NAME);
+    // day_one_main(FILE_NAME_DAY1);
+    day_two_main(FILE_NAME_DAY2);
 
     return 0;
 }
