978fa4cbe507506aa1ebe722d688d22488c22a7b
Alessandro Marchesini <149405466+marchesinia187179@users.noreply.github.com>
2025-12-04 19:14:56 +0100

    Implement the second part of the puzzle

--- aoc25/day2/day_two.c
+++ aoc25/day2/day_two.c
@@ -7,26 +7,61 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
-// Return the length of the ID
-int getLength(size_t ID) {
+#define MAX_ID_LENGTH 20    // I hypothesize the max ID length is 20
+
+int getIDLength(size_t ID) {
     size_t base = 10;
-    int length = 0;
     while (ID / base) {
-        length++;
         base *= 10;
     }
-    length++;
-    return length;
+    return (int) log10((double) base);
+}
+
+/* Check for section repetition
+ * The possible size of the section is between 1 and IDLength / 2,
+ * so sections greater than IDLength / 2 don't matter.
+ * The checking works like this:
+ *  - Take the i-th digit of the ID.
+ *  - Each of its repetitions is distant by a multiple of 'sec' (section size 'j').
+ *  - Check these repetitions without exceeding IDLength.
+ *  - Increase 'i' and check the other repetitions only if
+ *      - each check was successful, AND
+ *      - the number of repetition checks performed was equal to (IDLength - 1) / sec. */
+bool isInvalidSecondPartRec(int sec, int IDLength, char *ID) {
+    if (sec > IDLength / 2) return false;
+
+    bool ret = true;
+    for (int i = 0; i < sec; i++) {
+        int check = 0;
+        for (int j = sec + i; j < IDLength; j += sec) {
+            if (ID[j] != ID[i]) {
+                ret = false;
+                break;
+            }
+            check++;
+        }
+        if (check != (IDLength - 1) / sec) ret = false;
+        if (ret == false) break;
+    }
+
+    return ret || isInvalidSecondPartRec(sec + 1, IDLength, ID);
+}
+
+bool isInvalidSecondPart(size_t ID) {
+    char IDString[MAX_ID_LENGTH];
+    snprintf(IDString, sizeof(IDString), "%zd", ID);
+    return isInvalidSecondPartRec(1, getIDLength(ID), IDString);
 }
 
 /* If the ID length is odd, then the ID cannot be invalid according to the puzzle
  * Instead, if the ID length is even, it calculates half the ID length
  * and checks if the result of the division and the remainder are equal */
-bool isInvalid(size_t ID) {
-    int IDLength = getLength(ID);
+bool isInvalidFirstPart(size_t ID) {
+    int IDLength = getIDLength(ID);
     if (IDLength % 2L != 0) return false;
-
+    
     size_t base = (size_t) pow(10, (double) IDLength / 2);
     if (ID / base == ID % base) return true;
     return false;
@@ -34,7 +69,11 @@ bool isInvalid(size_t ID) {
 
 void checkInvalidID(size_t firstID, size_t lastID, size_t *sumInvalidID) {
     for (size_t i = firstID; i <= lastID; i++) {
-        if (isInvalid(i)) (*sumInvalidID) += i;
+        // The FOR check only this IF, if we want to verify the first part of the puzzle
+        // if (isInvalidFirstPart(i)) (*sumInvalidID) += i;
+
+        // The FOR check only this IF, if we want to verify the second part of the puzzle
+        if (isInvalidSecondPart(i)) (*sumInvalidID) += i;
     }
 }
 
@@ -46,7 +85,7 @@ int day_two_main(char *fileName) {
     }
 
     size_t firstID = 0, lastID = 0, sumInvalidID = 0;
-    bool isReadingSecondNuber = false;
+    bool isReadingSecondNumber = false;
     char c;
 
     while ((c = (char) fgetc(f)) != EOF) {
@@ -55,18 +94,18 @@ int day_two_main(char *fileName) {
 
             // Restore each variable to check a new ID range
             firstID = lastID = 0;
-            isReadingSecondNuber = false;
+            isReadingSecondNumber = false;
             continue;
         }
 
         if (c == '-') {     // Case the firstID is read
-            isReadingSecondNuber = true;
+            isReadingSecondNumber = true;
             continue;
         }
 
         // Save firstID and lastID of the current ID range
         if (c >= '0' && c <= '9') {
-            if (!isReadingSecondNuber) firstID = firstID * 10 + c - '0';
+            if (!isReadingSecondNumber) firstID = firstID * 10 + c - '0';
             else lastID = lastID * 10 + c - '0';
         }
     }
