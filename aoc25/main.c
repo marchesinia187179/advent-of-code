473fce344d6525068b02dac50665593d46ff2cd8
Alessandro Marchesini <149405466+marchesinia187179@users.noreply.github.com>
2025-12-15 00:39:00 +0100

    Add day four functionality to main.c

--- aoc25/main.c
+++ aoc25/main.c
@@ -1,14 +1,17 @@
 #include "./day1/day_one.h"
 #include "./day2/day_two.h"
 #include "day3/day_three.h"
+#include "day4/day_four.h"
 
 #define FILE_NAME_DAY1 "day1/input.txt"
 #define FILE_NAME_DAY2 "day2/input.txt"
 #define FILE_NAME_DAY3 "day3/input.txt"
+#define FILE_NAME_DAY4 "day4/input.txt"
 int main(void) {
     // day_one_main(FILE_NAME_DAY1);
     // day_two_main(FILE_NAME_DAY2);
-    day_three_main(FILE_NAME_DAY3);
+    // day_three_main(FILE_NAME_DAY3);
+    day_four_main(FILE_NAME_DAY4);
     
     return 0;
 }
