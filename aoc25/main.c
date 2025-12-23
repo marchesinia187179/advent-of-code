199c7fe1dcef774031a3ecfea4f4f6c68c2a6d54
Alessandro Marchesini <149405466+marchesinia187179@users.noreply.github.com>
2025-12-23 20:42:50 +0100

    Add day five function call and comment out day four

--- aoc25/main.c
+++ aoc25/main.c
@@ -2,16 +2,19 @@
 #include "./day2/day_two.h"
 #include "day3/day_three.h"
 #include "day4/day_four.h"
+#include "day5/day_five.h"
 
 #define FILE_NAME_DAY1 "day1/input.txt"
 #define FILE_NAME_DAY2 "day2/input.txt"
 #define FILE_NAME_DAY3 "day3/input.txt"
 #define FILE_NAME_DAY4 "day4/input.txt"
+#define FILE_NAME_DAY5 "day5/input.txt"
 int main(void) {
     // day_one_main(FILE_NAME_DAY1);
     // day_two_main(FILE_NAME_DAY2);
     // day_three_main(FILE_NAME_DAY3);
-    day_four_main(FILE_NAME_DAY4);
+    // day_four_main(FILE_NAME_DAY4);
+    day_five_main(FILE_NAME_DAY5);
     
     return 0;
 }
